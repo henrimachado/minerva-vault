f001cca77f208a3f873c0babc4007e6d61174144
Henrique Machado <henrimachado098@gmail.com>
2025-03-17 01:18:22 -0300

    chore: added front folders

--- minerva-vault-api/modules/users/validator/users_validator.py
+++ minerva-vault-api/modules/users/validator/users_validator.py
@@ -33,6 +33,13 @@ class UpdateUserValidator(serializers.Serializer):
     )
     
 class ChangePasswordValidator(serializers.Serializer):
+    user_id = serializers.UUIDField(
+        required=True, 
+        error_messages={
+            'required': 'O id do usuário é obrigatório',
+            'invalid': 'O id do usuário deve ser um UUID válido'
+        }
+    )
     current_password = serializers.CharField(
         required=True,
         error_messages={
